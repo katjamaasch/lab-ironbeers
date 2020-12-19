de07f29b2cadce5e91927ecdf43d52f6ff26a94e
Katja Maasch <katja.maasch@gmail.com>
2020-12-19 14:05:11 +0100

    Finished without bonus iterations

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const hbs = require('hbs');
+
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
@@ -14,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(__dirname + '/views/partials');
 
 // Add the route handlers here:
 
@@ -22,4 +23,32 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/:foo', (req, res) => {
+  const name = req.params.foo;
+  switch (name) {
+    case 'beers':
+      punkAPI
+        .getBeers()
+        .then(beersFromApi => {
+          res.render('beers', { beersList: beersFromApi });
+        })
+        .catch(error => console.log(error));
+
+      break;
+    case 'random-beers':
+      punkAPI
+        .getRandom()
+        .then(randomBeerFromApi => {
+          console.log(randomBeerFromApi);
+          res.render('random-beers', {
+            randomBeerFromApi: randomBeerFromApi[0]
+          });
+        })
+        .catch(error => console.log(error));
+      break;
+    default:
+      res.render('index');
+  }
+});
+
+app.listen(3100, () => console.log('🏃‍ on port 3100'));
